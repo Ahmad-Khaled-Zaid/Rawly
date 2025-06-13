b34e34b0e75ca379876e935efd1acdd50fe4e620
Ahmad Zaid <ahmadk.zaid97@gmail.com>
2025-06-13 04:16:35 +0300

    add email exception

---
+++ src/main/java/com/rawly/webapp/exception/InvalidEmailException.java
@@ -0,0 +1,7 @@
+package com.rawly.webapp.exception;
+
+public class InvalidEmailException extends RuntimeException {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
